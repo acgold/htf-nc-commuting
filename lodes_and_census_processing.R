f1f566f90e6ed0d4f8c3b2a76f08b1db8ef62fb7
Adam Gold <47038242+acgold@users.noreply.github.com>
2022-07-27 09:12:59 -0400

    Update lodes_and_census_processing.R
    - cleanup extra code

--- lodes_and_census_processing.R
+++ lodes_and_census_processing.R
@@ -34,10 +34,6 @@ nc_lodes_w_coords <- nc_lodes |>
   left_join(nc_blocks_coords |> select(h_geocode = GEOID, h_lat = Y, h_lon = X)) |> 
   mutate(row = row_number(), .before = w_geocode)
 
-# nc_lodes_w_coords |> 
-#   filter(h_geocode %in% nc_blocks$GEOID) |> 
-#   sfheaders::sf_linestring(x = c("h_lon", "w_long"), y = c("h_lat", "w_lat"), linestring_id = "row")
-
 # Filter out lodes data so we are left with only people who live within NC
 dt <- as.data.table(nc_lodes_w_coords |> 
                       filter(h_geocode %in% nc_blocks$GEOID))
@@ -45,76 +41,3 @@ dt <- as.data.table(nc_lodes_w_coords |>
 # Write these data to csv, so we can load it into Julia to query OSRM
 readr::write_csv(dt, "data/lodes.csv")
 
-
-# Extras
-z <- dt[
-  , {
-    geometry <- sf::st_linestring(x = matrix(c(h_lon, w_lon, h_lat, w_lat), nrow = 2, ncol = 2))
-    geometry <- sf::st_sfc(geometry, crs = 4269)
-    geometry <- sf::st_sf(geometry = geometry)
-  }
-  , by = row
-]
-
-z_sf <- sf::st_as_sf(z) |> 
-  left_join(nc_lodes_w_coords |> 
-              # slice(1:10000) |> 
-              select("row", "S000"))
-
-
-z_sf |> 
-  mapview(zcol = "S000", alpha = 0.1)
-
-
-library(osrm)
-
-library(foreach)
-
-b <- foreach(i=1:nrow(z_sf), .combine="bind_rows") %do%{
-  
-  request <- httr::GET(url = paste0("http://127.0.0.1:5000/route/v1/car/",z_sf$geometry[[i]][1],",",z_sf$geometry[[i]][3],";",z_sf$geometry[[i]][2],",",z_sf$geometry[[i]][4],"?alternatives=false&geometries=geojson&steps=false&overview=simplified"))
-  response <- jsonlite::fromJSON(rawToChar(request$content))
-  response$routes$geometry
-  
-  route <- osrm::osrmRoute(src = z_sf[i,] |> st_cast("POINT") |> slice(1),
-                           dst = z_sf[i,] |> st_cast("POINT") |> slice(2),
-                           osrm.server = "http://0.0.0.0:5000/",
-                           returnclass = "sf")
-  return(route)
-}
-
-z |> mapview()
-
-
-library(osmextract)
-
-osmextract::oe_vectortranslate(file_path = "/Volumes/my_hd/osrm/nc_osrm/north-carolina-latest.osm.pbf", layer = "other_relations")
-
-library(osmdata)
-
-beaufort_bb <- osmdata::getbb("Beaufort, North Carolina")
-
-
-building_csv <- read_csv("/Volumes/my_hd/osrm/NC_Buildings_Footprints_(2010).csv")
-
-test_routes <- read_csv("/Users/adam/Library/CloudStorage/OneDrive-SharedLibraries-UniversityofNorthCarolinaatChapelHill/Piehler Lab - Documents/Coastal Stormwater and SLR/people/analysis/data_downloads/full_test_routes.csv") |> 
-  mutate(w_geocode = as.character(w_geocode),
-         h_geocode = as.character(h_geocode))
-
-test_routes_sf <- test_routes |> sf::st_as_sf(wkt = "geom", crs = 4269)
-
-nc_blocks |> 
-  left_join(test_routes, by = c("GEOID" = "h_geocode")) |> 
-  group_by(GEOID, value) |> 
-  summarise(mean_duration = sum(S000, na.rm=T)) |> 
-  filter(value > 0) |> 
-  mapview(zcol = "mean_duration")
-library(leaflet)
-
-leaflet() %>%
-  addProviderTiles(provider = providers$CartoDB.DarkMatter) %>%
-  addGlPolylines(data = test_routes_sf, group = "lines")
-
-test_routes_sf |> 
-  # slice(1:1000) |> 
-  mapview()
